55872d7b592393672783564393a678cd2163803c
avimehenwal <avi.mehanwal@gmail.com>
2020-04-07 00:51:06 +0200

    :heavy_plus_sign: Fetch all images from assets
    dynamically add images from assets to page data

---
+++ pages/image.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-carousel>
+    <v-carousel-item
+      v-for="(item,i) in images"
+      :key="i"
+      :src="item.pathLong"
+      reverse-transition="fade-transition"
+      transition="fade-transition"
+    />
+    {{ item }}
+  </v-carousel>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      images: []
+    }
+  },
+  mounted () {
+    this.importAll(require.context('@/assets/images/', true, /\.png$/))
+  },
+  methods: {
+    importAll (r) {
+      r.keys().forEach(key => (this.images.push({ pathLong: r(key), pathShort: key })))
+    }
+  }
+}
+</script>
