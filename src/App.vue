478268a877d5dbefe637a2f276095904b261a6d6
avimehenwal <avi.mehanwal@gmail.com>
2020-04-11 10:13:13 +0200

    test vue-gsheet npm package

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,51 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <a href="https://www.npmjs.com/package/vue-gsheets" target="_blank">
+      <h1>Test vue-gsheets npm package</h1>
+    </a><br>
+
+    <table style="width:100%">
+      <caption>Data from public google sheets</caption>
+      <tr>
+        <th v-for="header in headers" :key="header">
+          {{ header }}
+        </th>
+      </tr>
+      <tr v-for="item in items" :key="item.dateStarted">
+        <td> {{ item.User }}        </td>
+        <td> {{ item.dateStarted }} </td>
+        <td> {{ item.Role }}        </td>
+      </tr>
+    </table>
+
+    <br>
+    <p>RECORDS : {{ records }}</p>
+    <p>COLUMNS : {{ COLUMNS }}</p>
+    <br>
+    <h2>RAW JSON</h2><br>
+    <div v-for="item in items" :key="item.dateStarted">
+      <p>
+        {{ item }}
+      </p>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { vueGsheets } from 'vue-gsheets'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  mixins: [vueGsheets],
+  name: 'App'
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.container {
+  padding: 10px
+}
+table, th, td {
+  border: 1px solid black;
+  padding: 15px;
 }
 </style>
