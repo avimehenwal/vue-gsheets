e78670dafb7362da521460f8b4f0993c82e80fb3
avimehenwal <avi.mehanwal@gmail.com>
2020-07-05 23:59:49 +0200

    feat(component): :white_check_mark: Add List componet
    with newlines at start and end

---
+++ components/List.vue
@@ -0,0 +1,41 @@
+<template>
+  <div> <br>
+    <v-card class="mx-auto" max-width="100%" tile>
+      <div v-for="item in items" :key="item">
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title>
+              <a :href="item.link" target="_blank">
+                {{ item.text }}
+              </a>
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </div>
+    </v-card>
+    <br>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'List',
+  props: {
+    items: {
+      type: Array,
+      default () {
+        return [
+          {
+            link: '',
+            text: 'text'
+          }
+        ]
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
